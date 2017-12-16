3b84d0821c38595a80ba3a4413d005cb5d80968d
Mustafa Bicer <mstfbiccer@gmail.com>
2017-12-17 02:02:54 +0300

    Git

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var app = express();
 var path = require('path');
-app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + './'));
 /**
  * @parameter segmentName
  * @parameter x_switch_user
